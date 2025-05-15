ae5a6f3e50c909672e5194ff2f5fcec2e796c3df
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-05-15 20:31:17 +0300

    Initial blueprint for Course model

--- src/AELP/courses/models.py
+++ src/AELP/courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+def get_upload_path(instance, filename):
+    return f"uploads/{filename}"
+
+
+class Course(models.Model):
+    class PublishStatus(models.TextChoices):
+        PUBLISHED = ('published', 'Published')
+        ARCHIVED = ('archived', 'Archived')
+        DRAFT = ('draft', 'Draft')
+
+    class AccessRequired(models.TextChoices):
+        ANYONE = ('anyone', 'Anyone')
+        EMAIL_REQUIRED = ('email_only', 'Email required')
+
+    title = models.CharField(max=100)
+    description = models.TextField(blank=True, null=True)
+    thumbnail_img = models.ImageField(
+        upload_to=get_upload_path,
+        blank=True,
+        null=True
+    )
+    access = models.CharField(
+        max_length=10,
+        choices=AccessRequired.choices,
+        default=AccessRequired.ANYONE
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+    )
